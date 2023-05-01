189460f2d87c087edf3d3c66e271d644df5ab77e
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-05-01 23:27:28 +0600

    client side ready to start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-5xl' >Hello</h1>
+      <h2>Hello</h2>
+      <h3>Hello</h3>
+    </>
+  )
+}
+
+export default App
