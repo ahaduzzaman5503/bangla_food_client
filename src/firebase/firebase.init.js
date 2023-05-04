69df1d4814ce03bbff86315c5c75402bf497ec46
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-05-04 15:06:23 +0600

    env.local file addded

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_IMgRM9GBoKUkOhhv1ry6wdHC6hFXvYc",
-  authDomain: "bd-cuisine.firebaseapp.com",
-  projectId: "bd-cuisine",
-  storageBucket: "bd-cuisine.appspot.com",
-  messagingSenderId: "285635476672",
-  appId: "1:285635476672:web:b90199be80acce1172c125"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
-
 const app = initializeApp(firebaseConfig);
 export default app;
